d67a9a90cf3e463e44e1b2352c7cd04996fbcb9c
Andrey <greemrock911@gmail.com>
2021-10-04 21:47:41 +0300

    feat: add morgan middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { morganMiddleware } from "./loggers/margan.logger";
 import { userRouter, groupRouter } from "./routers";
 
 export const app = express();
 
+app.use(morganMiddleware);
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/group", groupRouter);
