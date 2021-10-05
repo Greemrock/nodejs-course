9ce084787b348f4e58310da1eef5c0af31590735
Andrey <greemrock911@gmail.com>
2021-10-05 20:51:41 +0300

    feat: task-7-3

--- src/loggers/margan.logger.ts
+++ src/loggers/margan.logger.ts
@@ -4,7 +4,7 @@ import { Request } from "express";
 
 export const morganMiddleware = morgan(function (tokens, req: Request, res) {
   return [
-    chalk.hex("#ff4757").bold("🍄  Morgan --> "),
+    chalk.hex("#ff4757").bold("Morgan --> "),
     chalk.hex("#34ace0").bold(tokens.method(req, res)),
     chalk.hex("#ffb142").bold(tokens.status(req, res)),
     chalk.hex("#ff5252").bold(tokens.url(req, res)),
