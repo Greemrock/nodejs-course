ab10ec24fe3288e2fa02cc058650a8e8e3cbe022
Andrey <greemrock911@gmail.com>
2021-10-10 22:42:15 +0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 import { errorInternalServer, morganMiddleware } from "./middlewares";
 import { userRouter, groupRouter, loginRouter } from "./routers";
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morganMiddleware);
 app.use("/api/", loginRouter);
