d30809f5d45431046072fcd60a9450dd076f0515
Andrey <greemrock911@gmail.com>
2021-10-11 15:10:08 +0300

    refactor: change auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { winstonLogger } from "../loggers";
 
 import { SECRET_KEY } from "../shared/constant";
 import { HttpStatusCode } from "../utils";
@@ -9,19 +10,20 @@ export const authMiddleware = (
   res: Response,
   next: NextFunction
 ): void => {
-  const token = req.headers["x-access-token"] as string;
+  try {
+    const token = req.headers["x-access-token"] as string;
 
-  if (token) {
-    jwt.verify(token, SECRET_KEY, (err, decoded) => {
-      if (err) {
-        res
-          .status(HttpStatusCode.UNAUTHORIZED_ERROR)
-          .json("Failed to authenticate token.");
-      } else {
-        next();
-      }
-    });
-  } else {
-    res.status(HttpStatusCode.FORBIDDEN_ERROR).json("Unauthorized.");
+    if (token) {
+      jwt.verify(token, SECRET_KEY);
+      next();
+    } else {
+      res
+        .status(HttpStatusCode.UNAUTHORIZED_ERROR)
+        .json("Failed to authenticate token.");
+      winstonLogger.error("Failed to authenticate token.");
+    }
+  } catch (error) {
+    res.status(HttpStatusCode.FORBIDDEN_ERROR).send(error.message);
+    winstonLogger.error(`${error.name}: ${error.message}`);
   }
 };
